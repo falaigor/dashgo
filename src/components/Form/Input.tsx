0fdbd877e469778616cbcf694cacf3c563ded67a
Igor Santos <falaigors@gmail.com>
2021-05-02 01:28:29 -0300

    Validaçao de campos

--- src/components/Form/Input.tsx
+++ src/components/Form/Input.tsx
@@ -1,6 +1,8 @@
 import { forwardRef, ForwardRefRenderFunction } from 'react';
+import { FieldError } from 'react-hook-form';
 import {
   FormControl,
+  FormErrorMessage,
   FormLabel,
   Input as ChakraInput,
   InputProps as ChakraInputProps
@@ -9,12 +11,14 @@ import {
 interface InputProps extends ChakraInputProps {
   name: string;
   label?: string;
+  error?: FieldError;
 }
 
 const InputBase: ForwardRefRenderFunction<HTMLInputElement, InputProps>
-  = ({ name, label, ...rest }, ref) => {
+  = ({ name, label, error = null, ...rest }, ref) => {
+
     return (
-      <FormControl>
+      <FormControl isInvalid={!!error}>
         {!!label && <FormLabel htmlform={name}>{label}</FormLabel>}
 
         <ChakraInput
@@ -30,6 +34,12 @@ const InputBase: ForwardRefRenderFunction<HTMLInputElement, InputProps>
           ref={ref}
           {...rest}
         />
+
+        {!!error && (
+          <FormErrorMessage>
+            {error.message}
+          </FormErrorMessage>
+        )}
       </FormControl>
     );
   }
